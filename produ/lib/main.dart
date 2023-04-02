e800b246c2012f09aee1f8723b89ff8bee980067
shmaiii <luongxuanmai2109@gmail.com>
2023-04-02 01:18:40 -0700

    super bugged splashscreen lmao

--- produ/lib/main.dart
+++ produ/lib/main.dart
@@ -1,35 +1,48 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:produ/screens/Camera.dart';
 import 'package:camera/camera.dart';
 import 'screens/Camera.dart';
 import 'screens/MapScreen.dart';
-
+import 'screens/SplashScreen.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  //const MyApp({super.key});
+
+  @override 
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
+  bool splashing = true;
+
+  @override 
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 10), () {
+      setState(() {
+        splashing = false;
+      });
+    });
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: const MapScreen(/*title: 'Flutter Demo Home Page'*/),
+      home: splashing? SplashPage() : MapScreen(),
     );
   }
 }
